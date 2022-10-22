294d180156d02cec313aab147929003b5f32f507
Xuepo Ma <maxuepo@gmail.com>
2022-10-21 22:41:50 -0700

    refactor metrics; add a metric factory

--- example.jl
+++ example.jl
@@ -3,5 +3,5 @@ include("metrics.jl")
 scores = [[2, 1, 3], [1, 2, 3]]
 targets = [[1, 0, 0], [1, 0, 0]] 
 
-m1 = MRR("MRR@10")
-println(eval(m1, scores, targets, 10))
+m1 = MRR("MRR", scores, targets, 10)
+println(eval(m1))
--- metrics.jl
+++ metrics.jl
@@ -1,16 +1,36 @@
 abstract type Metric end
 
-mutable struct MRR <: Metric name :: String end
-mutable struct MAP <: Metric name :: String end
+mutable struct MRR 
+    name::String
+    list_of_scores::Vector
+    list_of_targets::Vector
+    top_n::Number
+end
+
+mutable struct MAP
+    name :: String
+    list_of_scores::Vector
+    list_of_targets::Vector
+    top_n::Number
+end
 
 
-function eval(metric::MRR, list_of_scores::Vector, list_of_targets::Vector, top_n::Integer=10)
+function metric_evaluator(name::String, list_of_scores::Vector, list_of_targets::Vector)
+    name_split = split(name, "@") 
+    if name_split[1] == "MRR"
+        return MRR(name_split[1], list_of_scores, list_of_targets, Integer(name_split))
+    else
+        return MAP(name_split[1], list_of_scores, list_of_targets, Integer(name_split))
+    end
+end
+
+function eval(m::MRR)
     s = 0
-    for (scores, targets) in zip(list_of_scores, list_of_targets)
+    for (scores, targets) in zip(m.list_of_scores, m.list_of_targets)
         sorted_scores = sortperm(-scores)
-        ranked_targets = targets[sorted_scores][begin:min(top_n, end)]
+        ranked_targets = targets[sorted_scores][begin:min(m.top_n, end)]
         s += 1//findall(x -> x != 0, ranked_targets)[1]
     end
-
-    return s / length(list_of_scores)
+    return s / length(m1.list_of_scores)
 end
+
